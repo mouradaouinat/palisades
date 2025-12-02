24f3dae94f68823e8fd24cf22981946e19cdcedc
Mourad Aouinat <mourad.eyes@gmail.com>
2025-12-02 19:45:09 +0100

    chore: fix name

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -194,7 +194,7 @@ export function Signup() {
               >
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                   <FormInput
-                    label="ISO Name"
+                    label="Name"
                     name="isoName"
                     type="text"
                     placeholder="Full name"
